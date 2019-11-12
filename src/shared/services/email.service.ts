de739c86a9f0bc12da2862eb8d6cc8792d6088ec
unknown <38155769+timtam8520@users.noreply.github.com>
2019-11-13 00:45:07 +1100

    feat: call default email provider from email service

--- src/shared/services/email.service.ts
+++ src/shared/services/email.service.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from 'express';
 import { ok, badRequest } from './api.service';
 import verifyRequest from './verifyRequest.service';
+import sendEmail from '../providers/default.provider';
 
-function trySendEmail(req: Request, res: Response) {
+async function trySendEmail(req: Request, res: Response) {
   try {
     const errors = verifyRequest(req.body);
     if (errors.length > 0) {
       return badRequest(res, { errors });
     }
-    return ok(res);
+    const result = await sendEmail(req.body);
+    return ok(res, result);
   } catch (error) {
     badRequest(res);
   }
